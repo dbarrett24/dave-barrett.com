8a49b4d018415d97b959b4b81023a0a12c159e95
dbarrett24 <dbarrett24@gmail.com>
2017-08-19 23:35:10 -0600

    project cards, animations, header animations, styling changes, responsiveness

--- dist/js/bundle.js
+++ dist/js/bundle.js
@@ -14,6 +14,51 @@ angular.module('app').controller('mainCtrl', function ($scope, mainService) {
     // *****************************
     $scope.test = mainService.test;
     // *****************************
+
+
+    $(document).ready(function () {
+
+        //Intro Header Scroll Fade Effect
+        var scrollPos = $(this).scrollTop();
+        console.log(scrollPos);
+
+        var fadeStart = 100 // 100px scroll or less will equiv to 1 opacity
+
+        ,
+            fadeUntil = 499 // 200px scroll or more will equiv to 0 opacity
+
+        ,
+            fading = $('#intro-header');
+
+        $(window).bind('scroll', function () {
+            var offset = $(document).scrollTop(),
+                opacity = 0;
+            if (offset <= fadeStart) {
+                opacity = 1;
+            } else if (offset <= fadeUntil) {
+                opacity = 1 - offset / fadeUntil;
+            }
+            fading.css('opacity', opacity);
+        });
+
+        //Projects CAROUSEL
+        $('.projects-carousel').flickity({
+            // options
+            cellAlign: 'left',
+            pageDots: true,
+            // groupCells: 3,
+            adaptiveHeight: false,
+            imagesLoaded: true,
+            autoPlay: false,
+            contain: true
+        });
+
+        $('.project-card').hover(function () {
+            $(this).addClass("hover");
+        }, function () {
+            $(this).removeClass("hover");
+        });
+    });
 });
 'use strict';
 
@@ -27,7 +72,53 @@ angular.module('app').service('mainService', function ($http) {
 angular.module('app').directive('navDir', function () {
     return {
         restrict: 'AE',
-        templateUrl: './views/directives/navDir.html'
+        templateUrl: './views/directives/navDir.html',
+        link: function link(scope, elem, attrs) {
+
+            //Smooth Scrolling
+            var scrollLink = $('.nav-links, .scroll-js');
+            // var scrollLink = $('.nav-links');
+            scrollLink.click(function (event) {
+                event.preventDefault();
+                $('body,html').animate({
+                    scrollTop: $(this.hash).offset().top - 100
+                }, 500);
+            });
+
+            //Sticky Nav
+            var targetPos = $('.nav').offset().top;
+            console.log(targetPos);
+
+            $(window).resize(function () {
+                targetPos = $('.nav').offset().top;
+            });
+
+            $(window).scroll(function () {
+                var scrollPos = $(this).scrollTop();
+                // console.log(scrollPos);
+
+                if (scrollPos > 879) {
+                    // $('.nav').removeClass('is-absolute');
+                    $('.nav').addClass('is-fixed');
+                } else {
+                    $('.nav').removeClass('is-fixed');
+                    // $('.nav').addClass('is-absolute');
+                }
+
+                scrollLink.each(function () {
+                    var sectionOffset = $(this.hash).offset().top - 250;
+
+                    if (sectionOffset <= scrollPos) {
+                        if ($(this).hasClass('scroll-js')) {
+                            $(this).css('background', 'transparent');
+                        } else {
+                            $(this).addClass('is-active');
+                            $(this).siblings().removeClass('is-active');
+                        }
+                    }
+                });
+            });
+        }
     };
 });
 //# sourceMappingURL=bundle.js.map
--- js/controllers/directives/navDir.js
+++ js/controllers/directives/navDir.js
@@ -1,6 +1,62 @@
 angular.module('app').directive('navDir', function(){
     return{
         restrict: 'AE',
-        templateUrl: './views/directives/navDir.html'
+        templateUrl: './views/directives/navDir.html',
+        link: function(scope, elem, attrs){
+
+
+            //Smooth Scrolling
+            var scrollLink = $('.nav-links, .scroll-js');
+            // var scrollLink = $('.nav-links');
+            scrollLink.click(function(event){
+                event.preventDefault();
+                $('body,html').animate({
+                    scrollTop: $(this.hash).offset().top - 100
+                }, 500)
+
+            })
+
+
+            //Sticky Nav
+            var targetPos = $('.nav').offset().top;
+            console.log(targetPos);
+
+            $(window).resize(function(){
+                targetPos = $('.nav').offset().top;
+            });
+
+            $(window).scroll(function(){
+                var scrollPos = $(this).scrollTop();
+                // console.log(scrollPos);
+
+                if(scrollPos > 879){
+                    // $('.nav').removeClass('is-absolute');
+                    $('.nav').addClass('is-fixed');
+                }
+                else{
+                    $('.nav').removeClass('is-fixed');
+                    // $('.nav').addClass('is-absolute');
+                }
+
+                scrollLink.each(function(){
+                    var sectionOffset = $(this.hash).offset().top - 250;
+
+                    if(sectionOffset <= scrollPos){
+                        if($(this).hasClass('scroll-js')){
+                            $(this).css('background', 'transparent');
+                            
+                        } else{
+                            $(this).addClass('is-active');
+                            $(this).siblings().removeClass('is-active');
+
+                        }
+                        
+                    }
+
+                })
+            })
+            
+
+        }
     }
 });
