2b6109e1a337846cde09e8891eacee4310266ad6
dbarrett24 <dbarrett24@gmail.com>
2017-09-10 09:18:36 -0600

    skills responsive?

--- dist/js/bundle.js
+++ dist/js/bundle.js
@@ -180,49 +180,6 @@ angular.module('app').controller('mainCtrl', function ($scope, mainService) {
     // *****************************
 
 });
-'use strict';
-
-angular.module('app').service('mainService', function ($http) {
-    // *****************************
-    this.test = "Controller & Service are working";
-    // *****************************
-    this.getSkills = function () {
-        return $http({
-            method: 'GET',
-            url: "../json/skills.json"
-        }).then(function (response) {
-            // console.log(response.data)
-            return response.data;
-        });
-    };
-    this.getExperience = function () {
-        return $http({
-            method: 'GET',
-            url: "../json/experience.json"
-        }).then(function (response) {
-            // console.log(response.data)
-            return response.data;
-        });
-    };
-    this.getQuotes = function () {
-        return $http({
-            method: 'GET',
-            url: "../json/quotes.json"
-        }).then(function (response) {
-            // console.log(response.data)
-            return response.data;
-        });
-    };
-    this.getProjects = function () {
-        return $http({
-            method: 'GET',
-            url: "../json/projects.json"
-        }).then(function (response) {
-            // console.log(response.data)
-            return response.data;
-        });
-    };
-});
 "use strict";
 
 /*
@@ -917,6 +874,49 @@ angular.module('app').service('mainService', function ($http) {
   }]);
 })(window, window.angular);
 //# sourceMappingURL=angular-animate.min.js.map
+'use strict';
+
+angular.module('app').service('mainService', function ($http) {
+    // *****************************
+    this.test = "Controller & Service are working";
+    // *****************************
+    this.getSkills = function () {
+        return $http({
+            method: 'GET',
+            url: "../json/skills.json"
+        }).then(function (response) {
+            // console.log(response.data)
+            return response.data;
+        });
+    };
+    this.getExperience = function () {
+        return $http({
+            method: 'GET',
+            url: "../json/experience.json"
+        }).then(function (response) {
+            // console.log(response.data)
+            return response.data;
+        });
+    };
+    this.getQuotes = function () {
+        return $http({
+            method: 'GET',
+            url: "../json/quotes.json"
+        }).then(function (response) {
+            // console.log(response.data)
+            return response.data;
+        });
+    };
+    this.getProjects = function () {
+        return $http({
+            method: 'GET',
+            url: "../json/projects.json"
+        }).then(function (response) {
+            // console.log(response.data)
+            return response.data;
+        });
+    };
+});
 'use strict';
 
 angular.module('app').directive('navDir', function () {
