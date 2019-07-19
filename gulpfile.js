145b2cbb6777879f54b331301908b194c4071a3d
root <root@DESKTOP-MV89OGF.localdomain>
2019-07-19 16:21:57 -0600

    gulp4 babel7 update. minor changes to index.js nodemon messaging

--- gulpfile.js
+++ gulpfile.js
@@ -3,66 +3,83 @@
 var gulp = require('gulp')
 ,   sourcemaps = require('gulp-sourcemaps')
 ,   sass = require('gulp-sass')
+,   babel = require('gulp-babel')
 ,   concat = require('gulp-concat')
 ,   CacheBuster = require('gulp-cachebust') //capitalized because it is a constructor function
 ,   print = require('gulp-print')
-,   babel = require('gulp-babel')
 
 var cachebust = new CacheBuster();  //need this constructor function
 
 
-gulp.task('build-css', function(){
+function buildCSS(){
+    console.log("Gulp: Rebuilding CSS with your changes");
     return gulp.src('./styles/*') //tell gulp to get EVERYTHING inside the styles folder.
-        .pipe(sourcemaps.init()) //pipe (take the results from the previous thing and do something). PIPE results to sourcemaps.init() function.
-        .pipe(sass()) //process SASS. Turn sass into CSS.
-        .pipe(cachebust.resources()) //Keep a copy of something and don't get the new thing. Blow up the old stuff, put in the new stuff!
-        .pipe(concat('styles.css')) //put all the sass files into ONE long file. Makes things faster
-        .pipe(sourcemaps.write('./maps')) //write out sourcemaps
-        .pipe(gulp.dest('./dist')); //stick the sourcemaps into the dist folder.
-})
+    .pipe(sourcemaps.init()) //pipe (take the results from the previous thing and do something). PIPE results to sourcemaps.init() function.
+    .pipe(sass()) //process SASS. Turn sass into CSS.
+    .pipe(cachebust.resources()) //Keep a copy of something and don't get the new thing. Blow up the old stuff, put in the new stuff!
+    .pipe(concat('styles.css')) //put all the sass files into ONE long file. Makes things faster
+    .pipe(sourcemaps.write('./maps')) //write out sourcemaps
+    .pipe(gulp.dest('./dist')); //stick the sourcemaps into the dist folder.
+}
 
 //Make sure to install all the tools listed above.
 // npm install --save gulp-sourcemaps gulp-sass gulp-concat -gulp-cachebust
 
 
 
-gulp.task('build-js', function() {
-   return gulp.src('js/**/*.js')               
-      .pipe(sourcemaps.init())
-      .pipe(print())                        
-      .pipe(babel({ presets: ['es2015'] }))
-      .pipe(concat('bundle.js'))
-      //.pipe(uglify())
-      .pipe(sourcemaps.write('./'))
-      .pipe(gulp.dest('./dist/js'));
-    });
-    
-    //npm install --save gulp-babel gulp-print babel-preset-es2015
-gulp.task('build-views', function(){
+function buildJS() {
+    console.log("Gulp: Rebuilding JavaScript with your changes");
+    return gulp.src('js/**/*.js')               
+    .pipe(sourcemaps.init())
+    .pipe(print())                        
+    .pipe(babel({ presets: ['@babel/preset-env'] }))
+    .pipe(concat('bundle.js'))
+    //.pipe(uglify())
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('./dist/js'));
+}
+
+//npm install --save gulp-babel gulp-print babel-preset-es2015
+function buildViews(){
+    console.log("Gulp: Rebuilding HTML with your changes");
     return gulp.src('views/**/*.html')
-        .pipe(gulp.dest('./dist/views'));
-})
-gulp.task('build-images', function(){
+    .pipe(gulp.dest('./dist/views'));
+    
+}
+function buildImages(){
+    console.log("Gulp: Rebuilding Images with your changes");
     return gulp.src('images/**/*')
-        .pipe(gulp.dest('./dist/images'));
-})
-gulp.task('build-JSON', function(){
+    .pipe(gulp.dest('./dist/images'));
+}
+function buildJSON(){
+    console.log("Gulp: Rebuilding JSON with your changes");
     return gulp.src('JSON/*.json')
-        .pipe(gulp.dest('./dist/JSON'));
-})
-gulp.task('build-download', function(){
+    .pipe(gulp.dest('./dist/JSON'));
+}
+function buildDL(){
+    console.log("Gulp: Rebuilding Downloads with your changes");
     return gulp.src('download/**/*')
-        .pipe(gulp.dest('./dist/download'));
-})
+    .pipe(gulp.dest('./dist/download'));
+}
 
-gulp.task('build', ['build-css', 'build-js', 'build-views', 'build-images', 'build-JSON', 'build-download'], function() {
+function build() {
     return gulp.src('index.html')
-        .pipe(cachebust.references())
-        .pipe(gulp.dest('dist'));
-});
+    .pipe(cachebust.references())
+    .pipe(gulp.dest('dist'));
+};
 
-gulp.task('watch', function() {
-    return gulp.watch(['./index.html','./views/**/*.html', './styles/*.*css', './js/**/*.js', './images/**/*', './JSON/*.json'], ['build']);
-});
+function watch() {
+    gulp.watch(['./index.html','./views/**/*.html'], buildViews);
+    gulp.watch('./styles/*.*css', buildCSS);
+    gulp.watch('./js/**/*.js', buildJS);
+    gulp.watch('./images/**/*', buildImages);
+    gulp.watch('./JSON/*.json', buildJSON);
+    gulp.watch('download/**/*', buildDL);
+};
 
-gulp.task('start', ['build', 'watch']);
+gulp.task("build", gulp.series(buildCSS, buildJS, buildViews, buildImages, buildJSON, buildDL, build));
+gulp.task('start', gulp.series(build, watch));
+
+gulp.task('default', function start(done){
+    done();
+});
--- index.js
+++ index.js
@@ -4,4 +4,4 @@ var app = express();
 
 app.use(express.static("./dist"));
 
-app.listen(8081);
+app.listen(8081, console.log("Listening on port 8081"));
